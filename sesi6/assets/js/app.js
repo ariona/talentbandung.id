132507f1ecd06a993aa5a5f31b52f7c45ab97b13
ariona <rian@ariona.net>
2017-11-19 21:30:18 +0700

    Update path, add  gallery caoursel & lightbox

--- sesi6/assets/js/app.js
+++ sesi6/assets/js/app.js
@@ -41,5 +41,39 @@ $(document).ready(function(){
          }
       });
    });
-   
+
+   var gallery = $('.gallery-carousel').slick({
+      slidesToShow : 3,
+      infinite     : false,
+      nextArrow: '<button type="button" class="slick-next"><i class="ion-android-arrow-forward"></i></button>',
+      prevArrow: '<button type="button" class="slick-prev"><i class="ion-android-arrow-back"></i></button>'
+   });
+
+   $('.test-popup').magnificPopup({
+      type: 'image',
+      closeOnContentClick: true,
+      mainClass: 'mfp-img-mobile',
+      image: {
+        verticalFit: true
+      },
+      gallery: {
+        enabled: true,
+        navigateByImgClick: true,
+        preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+      },
+		
+	});
+
+  //  function initMap() {
+  //     var uluru = {lat: -25.363, lng: 131.044};
+  //     var map = new google.maps.Map(document.getElementById('map'), {
+  //       zoom: 4,
+  //       center: uluru
+  //     });
+  //     var marker = new google.maps.Marker({
+  //       position: uluru,
+  //       map: map
+  //     });
+
+  //  }
 });
